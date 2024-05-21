2ba123e222da421f31847a48abbf42f4de8e21bc
Austin Kong <konghoitec@gmail.com>
2024-05-21 21:43:47 +0800

    Add render process controllers

---
+++ src/renderer/controllers/tasks.ts
@@ -0,0 +1,9 @@
+// In charge of managing the tasks store and its actions
+
+// Retrieve tasks from backend
+const retrieveTasks = async (): Promise<Task[]> => {
+}
+
+// Update the store and backend
+const updateTasks = async (tasks: Task[]): Promise<void> => {
+}
---
+++ src/renderer/controllers/timetable.ts
@@ -0,0 +1,11 @@
+// In charge of managing the timetable store and its actions
+
+// Retrieve timetable from backend
+const retrieveTimetable = async (): Promise<TimetableSlot[]> => {
+}
+
+// Passes NUS Mods url to backend to be retrieved from NUS Mods API
+// Then updates the store
+const uploadTimetable = async (url: string): Promise<TimetableSlot[]> => {
+
+}
