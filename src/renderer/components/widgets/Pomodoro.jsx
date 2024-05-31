35004591e9eab7d8aac3b8d967ce0ade4b47b8f5
Austin Kong <konghoitec@gmail.com>
2024-05-31 17:48:12 +0800

    Add pomodoro and timetable UI

--- src/renderer/components/widgets/Pomodoro.jsx
+++ src/renderer/components/widgets/Pomodoro.jsx
@@ -1,11 +1,49 @@
-import { GearSix } from '@phosphor-icons/react';
+import { IconContext, GearSix, FastForward, Play, Pause, Stop } from '@phosphor-icons/react';
 
 import { Widget, InteractionButton } from '@components/widgets/Widget';
+import '@styles/widgets/pomodoro.css';
+
+const FULL_DASH_ARRAY = 2 * Math.PI * 45;
 
 const Pomodoro = () => {
   return (
     <Widget className="pomodoro" title="Pomodoro" interaction={<InteractionButton text="Edit" icon={<GearSix />} />}>
-      Pomo
+      <div className="pomodoro-timer">
+        <svg className="pomodoro-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <g className="pomodoro-timer__circle">
+            <circle className="pomodoro-timer__path-elapsed" cx="50" cy="50" r="45" />
+            <path
+              strokeDasharray={`${(3 / 7) /* A fraction */ * FULL_DASH_ARRAY} ${FULL_DASH_ARRAY}`}
+              className="pomodoro-timer__path-remaining"
+              d="
+                  M 50, 50
+                  m -45, 0
+                  a 45,45 0 1,0 90,0
+                  a 45,45 0 1,0 -90,0
+                "
+            />
+          </g>
+        </svg>
+        <span className="pomodoro-timer__label">2:00</span>
+      </div>
+      <div className="pomodoro-controls">
+        <IconContext.Provider
+          value={{
+            size: 32,
+            weight: 'fill',
+          }}
+        >
+          <div className="pomodoro-controls__secondary">
+            <FastForward className="pomodoro-controls__skip" />
+          </div>
+          <div className="pomodoro-controls__main">
+            {false ? <Play className="pomodoro-controls__play" /> : <Pause className="pomodoro-controls__pause" />}
+          </div>
+          <div className="pomodoro-controls__secondary">
+            <Stop className="pomodoro-controls__stop" />
+          </div>
+        </IconContext.Provider>
+      </div>
     </Widget>
   );
 };
--- src/renderer/components/widgets/Timetable.jsx
+++ src/renderer/components/widgets/Timetable.jsx
@@ -1,17 +1,119 @@
-import { Upload } from '@phosphor-icons/react';
+import { useRef, useEffect } from 'react';
+import { Upload, CalendarBlank } from '@phosphor-icons/react';
 
+import { DaysOfWeek } from '@shared/constants';
 import { Widget, InteractionButton } from '@components/widgets/Widget';
 import '@styles/widgets/timetable.css';
 
+// Minutes since midnight in increments of 15
+const TIME = Array.from({ length: 96 + 1 }, (_, i) => i * 15);
+// 48px per 15 minutes
+const PX_PER_MINUTE = (48 * 4) / 60;
+
+const DUMMY_SLOTS = [
+  {
+    title: 'Maths',
+    description: 'Mathematics class @ Classroom A',
+    schedule: {
+      day: 'Monday',
+      start: 60,
+      end: 120,
+    },
+  },
+  {
+    title: 'Physics',
+    description: 'Physics class @ Classroom C',
+    schedule: {
+      day: 'Monday',
+      start: 360,
+      end: 550,
+    },
+  },
+  {
+    title: 'Chemistry',
+    description: 'Chemistry class @ Classroom B',
+    schedule: {
+      day: 'Tuesday',
+      start: 180,
+      end: 240,
+    },
+  },
+];
+
 const Timetable = () => {
+  const scrollableRef = useRef(null);
+  useEffect(() => {
+    const minutesSinceMidnight = new Date().getHours() * 60 + new Date().getMinutes();
+    // Subtract a default offset of 2 hours before
+    scrollableRef.current.scrollLeft = minutesSinceMidnight * PX_PER_MINUTE - PX_PER_MINUTE * 120;
+  });
+
   return (
     <Widget
       className="timetable"
-      title={'Timetable'}
+      title={'Schedule (Monday)'}
       interaction={<InteractionButton icon={<Upload />} text="Upload timetable" onClick={() => console.log('click')} />}
     >
-      Pending...
+      <div className="timetable__container">
+        <div className="timetable__days">
+          {DaysOfWeek.map((day) => (
+            <TimetableDay key={day} day={day} />
+          ))}
+        </div>
+        <div className="timetable__scrollable" ref={scrollableRef}>
+          <div className="timetable__hours">
+            {TIME.map((time) => (
+              <TimetableHour key={time} time={time} />
+            ))}
+          </div>
+          <div className="timetable__slots">
+            {DaysOfWeek.map((day) => (
+              <div key={day} className="timetable__slots-day">
+                {DUMMY_SLOTS.filter((slot) => slot.schedule.day === day).reduce((acc, curr, index, arr) => {
+                  return acc.concat(
+                    <TimetableSlot
+                      key={index}
+                      slot={curr}
+                      width={PX_PER_MINUTE * (curr.schedule.end - curr.schedule.start)}
+                      marginLeft={
+                        index === 0
+                          ? PX_PER_MINUTE * curr.schedule.start
+                          : PX_PER_MINUTE * (curr.schedule.start - arr[index - 1].schedule.end)
+                      }
+                    />
+                  );
+                }, [])}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </Widget>
+  );
+};
+
+const TimetableHour = ({ time }) => {
+  return (
+    <div className="timetable-hour">
+      <time className="timetable-hour__text">{time}</time>
+    </div>
+  );
+};
+
+const TimetableDay = ({ day }) => {
+  return (
+    <div className="timetable-day">
+      <span className="timetable-day__text">{day.slice(0, 3)}</span>
+    </div>
+  );
+};
+
+const TimetableSlot = ({ slot, width, marginLeft }) => {
+  return (
+    <div className="timetable-slot" style={{ width, marginLeft }}>
+      <CalendarBlank size="24px" />
+      <span className="timetable-slot__title">{slot.title}</span>
+    </div>
   );
 };
 
