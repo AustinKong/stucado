062d2ec47e85b1b3eeea1a6e67da37106028d8e6
Austin Kong <konghoitec@gmail.com>
2024-05-31 21:00:10 +0800

    Add insights UI

--- src/renderer/components/widgets/Insights.jsx
+++ src/renderer/components/widgets/Insights.jsx
@@ -1,6 +1,8 @@
 import { Widget, InteractionButton } from '@components/widgets/Widget';
 import { ArrowsClockwise } from '@phosphor-icons/react';
 
+import '@styles/widgets/insights.css';
+
 const Insights = () => {
   return (
     <Widget
@@ -8,7 +10,10 @@ const Insights = () => {
       title="Insights"
       interaction={<InteractionButton icon={<ArrowsClockwise />} text="Refresh" onClick={() => console.log('click')} />}
     >
-      <p>Coming soon...</p>
+      <div className="insights__content">
+        <p className="insights__main-text">Welcome back, it&apos;s time to get to work!</p>
+        <p className="insights__sub-text">Your productivity is up by 25% at this time of the day!</p>
+      </div>
     </Widget>
   );
 };
--- src/renderer/components/widgets/Pomodoro.jsx
+++ src/renderer/components/widgets/Pomodoro.jsx
@@ -8,41 +8,43 @@ const FULL_DASH_ARRAY = 2 * Math.PI * 45;
 const Pomodoro = () => {
   return (
     <Widget className="pomodoro" title="Pomodoro" interaction={<InteractionButton text="Edit" icon={<GearSix />} />}>
-      <div className="pomodoro-timer">
-        <svg className="pomodoro-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <g className="pomodoro-timer__circle">
-            <circle className="pomodoro-timer__path-elapsed" cx="50" cy="50" r="45" />
-            <path
-              strokeDasharray={`${(3 / 7) /* A fraction */ * FULL_DASH_ARRAY} ${FULL_DASH_ARRAY}`}
-              className="pomodoro-timer__path-remaining"
-              d="
-                  M 50, 50
-                  m -45, 0
-                  a 45,45 0 1,0 90,0
-                  a 45,45 0 1,0 -90,0
-                "
-            />
-          </g>
-        </svg>
-        <span className="pomodoro-timer__label">2:00</span>
-      </div>
-      <div className="pomodoro-controls">
-        <IconContext.Provider
-          value={{
-            size: 32,
-            weight: 'fill',
-          }}
-        >
-          <div className="pomodoro-controls__secondary">
-            <FastForward className="pomodoro-controls__skip" />
-          </div>
-          <div className="pomodoro-controls__main">
-            {false ? <Play className="pomodoro-controls__play" /> : <Pause className="pomodoro-controls__pause" />}
-          </div>
-          <div className="pomodoro-controls__secondary">
-            <Stop className="pomodoro-controls__stop" />
-          </div>
-        </IconContext.Provider>
+      <div className="pomodoro__content">
+        <div className="pomodoro-timer">
+          <svg className="pomodoro-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <g className="pomodoro-timer__circle">
+              <circle className="pomodoro-timer__path-elapsed" cx="50" cy="50" r="45" />
+              <path
+                strokeDasharray={`${(3 / 7) /* A fraction */ * FULL_DASH_ARRAY} ${FULL_DASH_ARRAY}`}
+                className="pomodoro-timer__path-remaining"
+                d="
+                    M 50, 50
+                    m -45, 0
+                    a 45,45 0 1,0 90,0
+                    a 45,45 0 1,0 -90,0
+                  "
+              />
+            </g>
+          </svg>
+          <span className="pomodoro-timer__label">2:00</span>
+        </div>
+        <div className="pomodoro-controls">
+          <IconContext.Provider
+            value={{
+              size: 32,
+              weight: 'fill',
+            }}
+          >
+            <div className="pomodoro-controls__secondary">
+              <FastForward className="pomodoro-controls__skip" />
+            </div>
+            <div className="pomodoro-controls__main">
+              {false ? <Play className="pomodoro-controls__play" /> : <Pause className="pomodoro-controls__pause" />}
+            </div>
+            <div className="pomodoro-controls__secondary">
+              <Stop className="pomodoro-controls__stop" />
+            </div>
+          </IconContext.Provider>
+        </div>
       </div>
     </Widget>
   );
