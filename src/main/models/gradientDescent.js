55f8064b85cb44a8732ffd31d584413231019b5c
Jing Le Ea <jinglemetta@gmail.com>
2024-06-02 09:38:02 +0800

    Add data points

--- src/main/models/gradientDescent.js
+++ src/main/models/gradientDescent.js
@@ -5,22 +5,29 @@ import { readDataPoints, updateDataPoint, deleteData } from '../database/databas
 //raw data for testing
 const rawData = `
 midnight Sunday 0 5 80.2819977
-night Friday 4 8 88.6510144
-evening Tuesday 3 5 49.9780856
-midnight Thursday 5 6 55.6206944
-earlyAfternoon Tuesday 2 1.5 81.1513711
-dawn Tuesday 3 0 28.1594180
-evening Saturday 0 6 89.2
-evening Thursday 5 5 62.7894415
-midnight Saturday 0 12 67.4273954
-lateMorning Sunday 0 2 47.0940917
-earlyAfternoon Monday 3 3 79.6783827
-lateAfternoon Saturday 0 5 65.0768531
-earlyAfternoon Wednesday 1 3 88.39
-night Thursday 5 7 62.7616772
+dawn Sunday 2 5 45.69
+lateMorning Sunday 0 2 87.0940917
+lateAfternoon Sunday 5 4 57.3437
 dawn Monday 3 7 17.8864896
+lateMorning Monday 1 2 77.389
+earlyAfternoon Monday 3 3 79.6783827
+earlyMorning Tuesday 0 1 108.289457
+earlyAfternoon Tuesday 2 1.5 81.1513711
+evening Tuesday 3 5 49.9780856
+dawn Tuesday 3 0 28.1594180
+earlyMorning Wednesday 0 1 120.940
+earlyAfternoon Wednesday 1 3 88.39
 night Wednesday 1 8 93.8474123
+dawn Thursday 2 3 65.9834
+midnight Thursday 5 6 75.6206944
+night Thursday 5 7 62.7616772
+evening Thursday 5 5 62.7894415
+earlyMorning Friday 0 2 93.85
+night Friday 4 8 88.6510144
 lateAfternoon Friday 4 7 67.2026135
+evening Saturday 0 6 89.2
+midnight Saturday 0 12 67.4273954
+lateAfternoon Saturday 0 5 65.0768531
 lateMorning Saturday 0 1 150.04
 `;
 
