e8db345b94ba3622b1e8e4342868167948e3955a
Jing Le Ea <jinglemetta@gmail.com>
2024-06-05 00:53:14 +0800

    Add best fit decreasing model

--- src/main/database/processCache.js
+++ src/main/database/processCache.js
@@ -1,16 +1,10 @@
-import { DaysOfWeek } from '../../shared/constants.js';
+import { DaysOfWeek, TimesOfDay } from '../../shared/constants.js';
 import { readTimetable, readTasks } from './cache.js';
 import { updateDataPoint } from './database.js';
 
 export function getTimeOfDay(hour) {
-  if (hour >= 6 && hour < 9) return 'earlyMorning';
-  if (hour >= 9 && hour < 12) return 'lateMorning';
-  if (hour >= 12 && hour < 15) return 'early afternoon';
-  if (hour >= 15 && hour < 18) return 'lateAfternoon';
-  if (hour >= 18 && hour < 21) return 'evening';
-  if (hour >= 21 && hour < 24) return 'night';
-  if (hour >= 0 && hour < 3) return 'midnight';
-  if (hour >= 3 && hour < 6) return 'dawn';
+  const index = Math.floor(hour / 3);
+  return TimesOfDay[index];
 }
 
 export function getHoursInClasses(timetable, taskStartTime) {
