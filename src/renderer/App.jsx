d15a67b85bd5979cecdbfd971151142b0b6c7800
Austin Kong <konghoitec@gmail.com>
2024-06-17 23:46:32 +0800

    Update onboarding to use CSS modules

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const hasOnboarded = useSelector((state) => state.settings.hasOnboarded);
   useRetrieveData();
 
-  if (!hasOnboarded) {
+  if (/*!hasOnboarded*/ true) {
     return <Onboarding />;
   }
 
---
+++ src/renderer/components/pages/Onboarding/Habits/index.jsx
@@ -0,0 +1,79 @@
+import { Sun, Moon, SunHorizon } from '@phosphor-icons/react';
+import { initializeModel } from '@services/insights';
+import LightThemeImage from '@assets/images/lightTheme.png';
+import { useState } from 'react';
+import styles from './styles.module.css';
+import Section from '@components/pages/Onboarding/Section';
+import Button from '@components/generic/Button';
+
+const Habits = ({ handleStep }) => {
+  const HABITS = [
+    {
+      id: 'earlyBird',
+      name: 'Early Bird',
+      description: 'You are most productive in the morning',
+      image: LightThemeImage,
+    },
+    {
+      id: 'afternoon',
+      name: 'Afternoon',
+      description: 'You are most productive in the afternoon',
+      image: LightThemeImage,
+    },
+    {
+      id: 'nightOwl',
+      name: 'Night Owl',
+      description: 'You are most productive at night',
+      image: LightThemeImage,
+    },
+  ];
+
+  const [selectedHabit, setSelectedHabit] = useState('afternoon');
+
+  const handlehabitChange = (habit) => {
+    setSelectedHabit(habit);
+  };
+
+  const handleSubmit = () => {
+    void initializeModel(selectedHabit);
+    handleStep();
+  };
+
+  return (
+    <Section
+      title="Set up your habits"
+      description="Set up your habits to get started with Stucado. This starts the model off with a basic understandinf of your study and productivity habits."
+    >
+      <h1>Set up your habits</h1>
+      <h3>Choose a productivity habit that best describes you.</h3>
+
+      <div className={styles.habits}>
+        {HABITS.map((habit) => (
+          <div
+            key={habit.id}
+            name={habit.id}
+            className={`${styles.habits__habit} ${selectedHabit === habit.id ? styles.habits__habitActive : ''}`}
+            onClick={() => handlehabitChange(habit.id)}
+          >
+            <div className={styles.habits__habitLeft}>
+              <span className={styles.habits__habitTitle}>
+                {habit.id === 'earlyBird' && <SunHorizon />}
+                {habit.id === 'afternoon' && <Sun />}
+                {habit.id === 'nightOwl' && <Moon />}
+                {habit.name}
+              </span>
+              <p className={styles.habits__habitDescription}>{habit.description}</p>
+            </div>
+            <img className={styles.habits__habitImage} src={habit.image} />
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.habits__footer}>
+        <Button onClick={handleSubmit}>Continue</Button>
+      </div>
+    </Section>
+  );
+};
+
+export default Habits;
